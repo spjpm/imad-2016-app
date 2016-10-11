2244df76e796aa9078a380442878973867118ff4
spjpm <santhoshprinceton@gmail.com>
2016-10-11 20:21:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title: 'Article-one',
+    heading:'Article-two'
+    content:
+    `
+     <p>
+        WordPress started in 2003 and is now the largest selfhosted
+blogging tool and is used on, literally, millions of
+sites worldwide. You are in good company if you use WordPress to publish
+on the web. Many famous blogs, news outlets, music
+sites, Fortune 500 companies and celebrities are using
+        </p>
+      <p>
+              WordPress started in 2003 and is now the largest selfhosted
+blogging tool and is used on, literally, millions of
+sites worldwide. You are in good company if you use WordPress to publish
+on the web. Many famous blogs, news outlets, music
+sites, Fortune 500 companies and celebrities are using
+        </p>
+    `
+}
+
+function createTemplate(data)
+var title=data.title;
+var heading=data.heading;
+var content=data.content;
+var htmlTemplate=
+    `
+    <html>
+  <head>
+    <title>
+      ${title} 
+      </title>
+      <meta name="viewport" content="width,initial-scale=1">
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+  <body>
+      <div>
+          <a href="/">Home</a>
+          </div>
+           <hr/>
+    <div>
+     <h3> ${heading}</h3>
+      </div>
+      <hr/>
+    <div>
+      ${content}
+      </div>
+    </body>
+</html>
+`
+;return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne)));
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));

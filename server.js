889e93816c20cbef984b8509c043ef9b0f24ecb6
spjpm <santhoshprinceton@gmail.com>
2016-11-10 21:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,12 +55,12 @@ var htmlTemplate=
   <head>
     <title>
       ${title} 
-      </title>
+    </title>
       <meta name="viewport" content="width,initial-scale=1">
        <link href="/ui/style.css" rel="stylesheet" />
        <link href="/ui/style.css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>My Webpage</title>
+
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <style>
